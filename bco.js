bffa3c2c27de23284f7c847c8dfc24513313e6c3
Kevin R. Sherman <kevin.r.sherman@gmail.com>
2016-04-23 13:41:25 -0400

    about to add bee

--- bco.js
+++ bco.js
@@ -1,3 +1,36 @@
+var Bee = (function () {
+    function Bee() {
+    }
+    return Bee;
+}());
+var Hive = (function () {
+    function Hive(totalNumberBees, numberInactive, numberActive, numberScout, maxNumberVisits, maxNumberCycles, citiesData) {
+    }
+    // Hive data fields here
+    Hive.prototype.ToString = function () {
+        return null;
+    };
+    Hive.prototype.GenerateRandomMemoryMatrix = function () {
+        return null;
+    };
+    Hive.prototype.GenerateNeighborMemorMatrix = function (memoryMatrix) {
+        return null;
+    };
+    Hive.prototype.MeasureOfQuality = function (memoryMatrix) {
+        return 0;
+    };
+    Hive.prototype.Solve = function (showProgress) {
+    };
+    Hive.prototype.ProcessActiveBee = function (i) {
+    };
+    Hive.prototype.ProcessScoutnBee = function (i) {
+    };
+    Hive.prototype.ProcessInactiveBee = function (i) {
+    };
+    Hive.prototype.DoWaggleDance = function (i) {
+    };
+    return Hive;
+}());
 var CitiesData = (function () {
     function CitiesData(numberCities) {
         this.start = 65;
@@ -35,15 +68,51 @@ var CitiesData = (function () {
     };
     return CitiesData;
 }());
-function test2() {
-    var cd = new CitiesData(10);
-    for (var i = 0; i < cd.cities.length; i++) {
+/* TEST
+function test() {
+    var cd : CitiesData = new CitiesData(10);
+    
+    for(var i = 0; i < cd.cities.length; i++){
         console.log("CITY: " + i + " " + cd.cities[i]);
     }
+    
     var x = cd.Distance(cd.cities[3], cd.cities[2]);
-    console.log("DISTANCE: " + x);
+    console.log("DISTANCE (D->C):  " + x);
+    
+    var y = cd.Distance(cd.cities[2], cd.cities[3]);
+    console.log("DISTANCE (C->D):  " + y);
+    
     console.log("SHORTEST: " + cd.ShortestPathLength());
-    console.log("NUM Paths: " + cd.NumberOfPossiblePaths());
+    console.log("NUM Paths: " + cd.NumberOfPossiblePaths())
     console.log("TOSTRING: " + cd.ToString());
 }
-test2();
+
+test(); */
+/*
+In terms of ratios, there's some research that suggests the best percentages of active, inactive and scout bees are often
+roughly 75 percent, 10 percent and 15 percent respectively.
+ */
+function main(args) {
+    console.log("\nBegin Simulated Bee Colony algorithm demo\n");
+    console.log("Loading cities data for SBC Traveling Salesman Problem analysis");
+    var citiesData = new CitiesData(20);
+    console.log(citiesData.ToString());
+    console.log("Number of cities: " + citiesData.cities.length);
+    console.log("Number of possible paths: " + citiesData.NumberOfPossiblePaths().toLocaleString());
+    console.log("Best possible solution (shortest path) length = " + citiesData.ShortestPathLength().toLocaleString());
+    var totalNumberBees = 100;
+    var numberInactive = 20;
+    var numberActive = 50;
+    var numberScout = 30;
+    var maxNumberVisits = 100;
+    var maxNumberCycles = 3460;
+    var hive = new Hive(totalNumberBees, numberInactive, numberActive, numberScout, maxNumberVisits, maxNumberCycles, citiesData);
+    console.log("\nInitial random hive");
+    console.log(hive.ToString());
+    var doProgressBar = true;
+    hive.Solve(doProgressBar);
+    console.log("\nFinal hive");
+    console.log(hive.ToString());
+    console.log("End Simulated Bee Colony demo");
+}
+main(null);
--- bco.ts
+++ bco.ts
@@ -1,3 +1,52 @@
+class Bee {
+    
+}
+
+class Hive {
+    
+    // Hive data fields here
+    ToString(): string {
+        return null;
+    }
+    
+    constructor(totalNumberBees: number, numberInactive: number, numberActive: number, 
+                numberScout: number, maxNumberVisits: number, maxNumberCycles: number, 
+                citiesData: CitiesData) {
+    }
+    
+    GenerateRandomMemoryMatrix(): string[]{
+        return null;
+    }
+    
+    GenerateNeighborMemorMatrix(memoryMatrix: string[]): string[]{
+        return null;
+    }
+    
+    MeasureOfQuality(memoryMatrix: string[]): number {
+        return 0;
+    }
+    
+    Solve(showProgress: boolean): void{
+        
+    }
+    
+    ProcessActiveBee(i: number) : void{
+        
+    }
+    
+    ProcessScoutnBee(i: number) : void{
+        
+    } 
+    
+    ProcessInactiveBee(i: number) : void{
+        
+    }
+    
+    DoWaggleDance(i: number) : void{
+        
+    }
+}
+
 class CitiesData {
     public cities: string[];
     public start: number = 65;
@@ -41,7 +90,8 @@ class CitiesData {
     }
 }
 
-function test2() {
+/* TEST 
+function test() {
     var cd : CitiesData = new CitiesData(10);
     
     for(var i = 0; i < cd.cities.length; i++){
@@ -49,11 +99,52 @@ function test2() {
     }
     
     var x = cd.Distance(cd.cities[3], cd.cities[2]);
-    console.log("DISTANCE: " + x);
+    console.log("DISTANCE (D->C):  " + x);
+    
+    var y = cd.Distance(cd.cities[2], cd.cities[3]);
+    console.log("DISTANCE (C->D):  " + y);
     
     console.log("SHORTEST: " + cd.ShortestPathLength());
     console.log("NUM Paths: " + cd.NumberOfPossiblePaths())
     console.log("TOSTRING: " + cd.ToString());
 }
 
-test2();
+test(); */
+
+/*
+In terms of ratios, there's some research that suggests the best percentages of active, inactive and scout bees are often
+roughly 75 percent, 10 percent and 15 percent respectively.
+ */
+
+function main(args: string[]):void {
+    console.log("\nBegin Simulated Bee Colony algorithm demo\n");
+    console.log("Loading cities data for SBC Traveling Salesman Problem analysis");
+    var citiesData: CitiesData = new CitiesData(20);
+    console.log(citiesData.ToString());
+    console.log("Number of cities: " + citiesData.cities.length);
+    console.log("Number of possible paths: " + citiesData.NumberOfPossiblePaths().toLocaleString());
+    console.log("Best possible solution (shortest path) length = " + citiesData.ShortestPathLength().toLocaleString());
+    
+    var totalNumberBees : number = 100;
+    var numberInactive : number = 20;
+    var numberActive : number = 50;
+    var numberScout : number = 30;
+    var maxNumberVisits : number = 100;
+    var maxNumberCycles : number = 3460;
+    
+    var hive: Hive = new Hive(totalNumberBees, numberInactive, numberActive, numberScout, maxNumberVisits, maxNumberCycles, citiesData);
+    
+    console.log("\nInitial random hive");
+    console.log(hive.ToString());
+    
+    var doProgressBar: boolean = true;
+    hive.Solve(doProgressBar);
+    
+    console.log("\nFinal hive");
+    console.log(hive.ToString());
+    
+    
+    console.log("End Simulated Bee Colony demo");
+}
+
+main(null);
